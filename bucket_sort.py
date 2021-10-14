34182e6447de304ba23c1bdd75eae571c5f274d2
William <wgkwilliam@gmail.com>
2021-10-14 20:33:51 -0300

    Corrigindo nome de variáveis e adicionando comentários em PTBR

--- bucket_sort.py
+++ bucket_sort.py
@@ -1,30 +1,30 @@
 def bucket_sort(valores):
-    # Find maximum value in the list and use length of the list to determine which value in the list goes into which bucket 
-    Maior_Elemento = max(valores)
-    tamanho = Maior_Elemento/len(valores)
+    # Encontra o valor máximo na lista e use o comprimento da lista para determinar qual valor da lista vai para qual bucket 
+    maior_elemento = max(valores)
+    tamanho = maior_elemento/len(valores)
 
-    # Create n empty buckets where n is equal to the length of the input list
-    Lista_Buckets= []
+    # Cria n baldes vazios onde n é igual ao tamanho da lista 
+    lista_buckets= []
     for x in range(len(valores)):
-        Lista_Buckets.append([]) 
+        lista_buckets.append([]) 
 
-    # Put list elements into different buckets based on the tamanho
+    # Coloca os elementos da lista em diferentes grupos com base no tamanho
     for i in range(len(valores)):
         auxiliar = int(valores[i] / tamanho)
         if auxiliar != len (valores):
-            Lista_Buckets[auxiliar].append(valores[i])
+            lista_buckets[auxiliar].append(valores[i])
         else:
-            Lista_Buckets[len(valores) - 1].append(valores[i])
+            lista_buckets[len(valores) - 1].append(valores[i])
 
-    # Sort elements within the buckets using Insertion Sort
+    # Ordena os elementos dentro dos buckets
     for z in range(len(valores)):
-        insertion_sort(Lista_Buckets[z])
+        insertion_sort(lista_buckets[z])
             
-    # Concatenate buckets with sorted elements into a single list
-    Lista_Ordenada = []
+    # Concatena os buckets ordenados em uma unica lista
+    lista_ordenada = []
     for x in range(len (valores)):
-        Lista_Ordenada = Lista_Ordenada + Lista_Buckets[x]
-    return Lista_Ordenada
+        lista_ordenada = lista_ordenada + lista_buckets[x]
+    return lista_ordenada
 
 def insertion_sort(valores):
     n = len(valores)
@@ -39,10 +39,10 @@ def insertion_sort(valores):
 
 def main():
     valores = [1.20, 0.22, 0.43, 0.36, 0.39, 0.27, 100, 2320, 500, 1000234]
-    print('ORIGINAL LIST:')
+    print('LISTA ORIGINAL:')
     print(valores)
-    sorted_list = bucket_sort(valores)
-    print('SORTED LIST:')
-    print(sorted_list)
+    lista_ordenada = bucket_sort(valores)
+    print('LISTA ORDENADA:')
+    print(lista_ordenada)
 
 main()
